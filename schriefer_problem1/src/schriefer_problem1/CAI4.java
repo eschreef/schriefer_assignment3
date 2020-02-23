a9f8ab7fb4451e340b22f46ed51266192ebeac37
Erik Schriefer <eschreef@knights.ucf.edu>
2020-02-23 14:39:27 -0500

    First commit

---
+++ schriefer_problem1/src/schriefer_problem1/CAI4.java
@@ -0,0 +1,48 @@
+package schriefer_problem1;
+
+import java.security.SecureRandom;
+import java.util.Scanner;
+
+public class CAI4 
+{
+	public static void main(String[] args) 
+	{
+		//use a secure random object to produce 2 digits
+				SecureRandom rand = new SecureRandom();
+				int one = rand.nextInt(9) + 1;
+				int two = rand.nextInt(9) + 1;
+				
+				//print out a question for the user
+				System.out.printf("How much is %d times %d? ", one, two);
+				
+				//determine the actual answer
+				int answer = one*two;
+				
+				//let the user answer the question
+				Scanner scan = new Scanner(System.in);
+				int userAnswer = scan.nextInt();
+				
+				//check their answer
+				if(answer == userAnswer)
+				{
+					System.out.println("Very good!");
+					scan.close();
+					System.exit(answer);
+				}
+				else
+				{
+					while(answer != userAnswer)
+					{
+						System.out.print("No. please try again: ");
+						userAnswer = scan.nextInt();
+						if(answer == userAnswer)
+						{
+							System.out.println("Very good!");
+							scan.close();
+							System.exit(answer);
+						}
+				}
+		}
+
+	}
+}
---
+++ schriefer_problem2/src/schriefer_problem2/SavingsAccount.java
@@ -0,0 +1,6 @@
+package schriefer_problem2;
+
+public class SavingsAccount 
+{
+
+}
